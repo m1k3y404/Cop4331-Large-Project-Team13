ecbf99b991e17e158b089cb290eee365f79d7a1c
Ben Williams <40040345+benw23@users.noreply.github.com>
2026-04-03 17:42:57 -0400

    frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,20 @@ import './index.css'
 import { BrowserRouter, Route } from 'react-router'
 import { Routes } from 'react-router'
 import { Login } from './routes/Login.tsx'
+import { Register } from './routes/Register.tsx'
+import { Index } from './routes/Index.tsx'
+import { VerifyEmail } from './routes/VerifyEmail.tsx'
+import { Feed } from './routes/Feed.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}></Route>
+        <Route path="/" element={<Index />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/verify-email" element={<VerifyEmail />}></Route>
+        <Route path="/feed" element={<Feed />}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
