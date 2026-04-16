9bf95a4116dac5998b91d80a1c0801e4c3be2e73
Ben Williams <40040345+benw23@users.noreply.github.com>
2026-04-16 00:39:44 -0400

    nav for editor

--- frontend/src/routes/Editor.tsx
+++ frontend/src/routes/Editor.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import { Layout } from 'antd';
 import PostEditor from '../components/Editor/PostEditor';
+import { Content } from 'antd/es/layout/layout';
+import Nav from '../components/Layout/Nav';
 
-export const Editor: React.FC = () => {
-  return <PostEditor />;
-};
+
+export function Editor() {
+    return (
+        <Layout style={{ background: "var(--bg)", minHeight: "100dvh", overflowX: "hidden" }}>
+            <Content>
+                <Nav />
+                <PostEditor />
+            </Content>
+        </Layout>
+    );
+}
 
 export default Editor;
